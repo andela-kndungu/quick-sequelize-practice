3d7b5ebcd5d1c6bb93ced139a0d59580e735f012
Kinuthia Ndung'u <kinuthia.ndungu@andela.com>
2016-09-21 10:00:40 +0300

    Implement CRUD

--- server/index.js
+++ server/index.js
@@ -27,6 +27,47 @@ User.sync({ force: true }).then(() => {
   });
 });
 
+app.get('/data', (req, res) => {
+  User.findOne({
+    where: { lastName: 'Hancock' },
+  }).then((project) => {
+    // project will be the first entry of the Projects table with the title 'aProject' || null
+    // project.title will contain the name of the project
+    res.json(project);
+  });
+});
+
+app.put('/data', (req, res) => {
+  User.update(
+    {
+      firstName: 'James'
+    },
+    {
+      where: { firstName: 'John' }
+    }
+  )
+    .then((result) => {
+      res.send(result);
+    }, (rejectedPromiseError) => {
+      console.log(rejectedPromiseError);
+    });
+});
+
+app.delete('/data', (req, res) => {
+  User.destroy(
+    {
+      where: { firstName: 'John' }
+    }
+  )
+    .then((result) => {
+      if (result) {
+        res.send('Entry deleted');
+      } else {
+        res.send('Entry not found');
+      }
+    });
+});
+
 app.listen(3000, () => {
   console.log('Server listening at port 3000');
 });
