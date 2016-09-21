cc3dee545bf2753bd3932542329fabda4f8db4ce
Kinuthia Ndung'u <kinuthia.ndungu@andela.com>
2016-09-21 10:49:54 +0300

    Add Material UI

--- client/components/Main.jsx
+++ client/components/Main.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import RaisedButton from 'material-ui/RaisedButton';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
 
-const Main = () => {
-  return (
-    <h1>Hello World</h1>
-  );
-};
+const muiTheme = getMuiTheme({
+  palette: {
+    primary1Color: '#607D8B',
+    accent1Color: '#00BCD4',
+  },
+});
+
+const Main = () => (
+  <MuiThemeProvider muiTheme={muiTheme}>
+    <RaisedButton label="Hello World" primary />
+  </MuiThemeProvider>
+);
 
 export default Main;
 
